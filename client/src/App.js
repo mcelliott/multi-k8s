1a463bd0e1de267e0160b9e26440c591f82ab6a4
Matt Elliott <mcelliott80@gmail.com>
2020-04-11 12:20:54 +1000

    update component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
+
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
+import React, { Component } from 'react';
+
 import Fib from './Fib';
+import OtherPage from './OtherPage';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
@@ -12,7 +14,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
